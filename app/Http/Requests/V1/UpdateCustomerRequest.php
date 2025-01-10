4e1f19b3bdf1126f1c2d5859f6b49f9079e3bb82
anharsaja <anharmuk12@gmail.com>
2025-01-11 03:57:03 +0700

    :sparkles: feat: manupulating data(customers): PUT - PATCH API

--- app/Http/Requests/V1/UpdateCustomerRequest.php
+++ app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCustomerRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -19,10 +20,39 @@ class UpdateCustomerRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+    public function rules()
     {
-        return [
-            //
-        ];
+        $method = $this->method();
+
+        if ($method == 'PUT'){
+            return [
+                'name' => ['required'],
+                'type' => ['required', Rule::in(['I', 'B', 'i', 'b'])],
+                'email' => ['required','email'],
+                'address' => ['required'],
+                'city'=> ['required'],
+                'state'=> ['required'],
+                'postalCode'=> ['required'],
+            ];
+        } else {
+            return [
+                'name' => [ 'sometimes', 'required'],
+                'type' => [ 'sometimes', 'required', Rule::in(['I', 'B', 'i', 'b'])],
+                'email' => [ 'sometimes', 'required','email'],
+                'address' => [ 'sometimes', 'required'],
+                'city'=> [ 'sometimes', 'required'],
+                'state'=> [ 'sometimes', 'required'],
+                'postalCode'=> [ 'sometimes', 'required'],
+            ];
+        }
+    }
+
+    protected function prepareForValidation()
+    {
+        if ($this->postalCode) {
+            $this->merge([
+                'postal_code' => $this->postalCode
+            ]);
+        }
     }
 }
